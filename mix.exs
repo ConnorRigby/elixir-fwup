c511d5b1e839e24bb88a520830a735aeefc74373
connor rigby <konnorrigby@gmail.com>
2019-09-18 13:51:51 -0700

    Release v0.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fwup.MixProject do
   def project do
     [
       app: :fwup,
-      version: "0.3.0",
+      version: "0.4.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
