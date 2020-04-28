d6fde10bab63bc2bbac4a63d552a00203d9c2984
Frank Hunleth <fhunleth@troodon-software.com>
2020-04-28 10:25:13 -0400

    Bump dialyxir to 1.0.0

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Fwup.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.19", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.6", only: :dev, runtime: false}
+      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false}
     ]
   end
 
