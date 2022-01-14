7cbd87bb3891f23cf73999b193e371e6bb36fd42
Connor Rigby <connorr@hey.com>
2022-01-14 14:04:46 -0700

    Add apply/4 function
    Allows calling fwup without needing a stream

--- test/fwup_test.exs
+++ test/fwup_test.exs
@@ -1,4 +1,12 @@
 defmodule FwupTest do
   use ExUnit.Case
   doctest Fwup
+
+  alias Fwup.TestSupport.Fixtures
+
+  test "apply" do
+    {:ok, fw} = Fixtures.create_firmware("test_apply")
+    dev = Path.join(Path.dirname(fw), "test_apply.img")
+    assert :ok = Fwup.apply(dev, "complete", fw)
+  end
 end
