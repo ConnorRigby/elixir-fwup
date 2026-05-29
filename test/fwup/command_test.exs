0ce6b3394c2c653161a92ae1bc11463dd9e3a5a1
Frank Hunleth <fhunleth@troodon-software.com>
2026-05-29 06:46:31 -0400

    Fix unsafe String.to_atom/1 and add test

--- test/fwup/command_test.exs
+++ test/fwup/command_test.exs
@@ -17,6 +17,9 @@ defmodule Fwup.CommandTest do
     # See fwup 1.14.0 release notes for UUID change
     assert metadata["uuid"] == "dfd76641-2d09-5a0b-043e-536d28640b29" or
              metadata["uuid"] == "f46925cb-7c7b-5aae-10fb-a93c09941b60"
+
+    # fwup 1.15.0 reports nicknames
+    assert metadata["nickname"] in [nil, "street-spy"]
   end
 
   test "metadata converts keys to atoms" do
@@ -33,5 +36,19 @@ defmodule Fwup.CommandTest do
 
     assert metadata.uuid == "dfd76641-2d09-5a0b-043e-536d28640b29" or
              metadata.uuid == "f46925cb-7c7b-5aae-10fb-a93c09941b60"
+
+    # fwup 1.15.0 reports nicknames
+    assert metadata[:nickname] in [nil, "street-spy"]
+  end
+
+  test "all metadata keys can be converted to atoms" do
+    {:ok, fw_path} = Fixtures.create_firmware("test")
+    {:ok, atom_metadata} = Fwup.Command.metadata(fw_path, keys_to_atoms: true)
+    {:ok, string_metadata} = Fwup.Command.metadata(fw_path)
+
+    num_atom_keys = length(Map.keys(atom_metadata))
+    num_string_keys = length(Map.keys(string_metadata))
+
+    assert num_atom_keys == num_string_keys
   end
 end
